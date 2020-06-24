7780fe763723e3f66255795fc7461f00a4356b47
Frits van der Zalm <f.vd.zalm@live.nl>
2020-06-24 22:47:09 +0200

    v1.0 full functionality

--- apps/python/trackconditions/trackconditions.py
+++ apps/python/trackconditions/trackconditions.py
@@ -20,15 +20,37 @@ os.environ['PATH'] = os.environ['PATH'] + ";."
 
 from trackconditions_lib.sim_info import info
 
-# Initialize static variables.
-APP_HEIGHT = 150
-APP_ASPECT_RATIO = 2
-BACKGROUND_OPACITY = 0.5
+# Start configparser
+config = configparser.ConfigParser()
+configfile = "apps/python/trackconditions/config.ini"
+config.read(configfile)
 
-# Initialize empty dict in which all fetched Assetto Corsa data is stored
+# Initialize app_info, dict in which basic app info is stored.
+app_info = {}
+app_info['name'] = "Track Conditions"
+
+try:
+    app_info['height'] = config.getint('GENERAL', 'app_height')
+except Exception as error:
+    app_info['height'] = 150
+    ac.log("{}: app_height Error: {}".format(app_info['name'], str(error)))
+
+app_info['aspect_ratio'] = 2.25
+app_info['title'] = "" 
+app_info['padding'] = 0.1
+app_info['width'] = app_info['height'] * app_info['aspect_ratio']
+
+# Initialize empty dict in which the text labels are stored.
+labels = {}
+
+# Initialize empty dict in which fetched Assetto Corsa data is stored.
 ac_data = {}
 ac_data['wind_speed'] = 10
+ac_data['track_grip'] = 0
+ac_data['road_temp'] = 0
+ac_data['air_temp'] = 0
 
+# Initialize empty dict in which wind arrow indicator data is stored.
 arrow = {}
 
 # Timers
@@ -36,50 +58,77 @@ data_timer_60_hz = 0
 data_timer_0p1_hz = 8
 calc_timer_60_hz = 0
 
-
 def acMain(ac_version):
     """Run upon startup of Assetto Corsa. """
-    global APP_HEIGHT, APP_ASPECT_RATIO
-    global label_wind_direction, label_car_direction, label_rel_direction
+    global app_info, app_window
+    global labels
 
-    app_window = ac.newApp("Track Conditions")
-    ac.setSize(app_window, (APP_ASPECT_RATIO * APP_HEIGHT), APP_HEIGHT)
-    ac.setBackgroundOpacity(app_window, BACKGROUND_OPACITY)
-    ac.setTitle(app_window, "")
+    # Initialize app window
+    app_window = ac.newApp(app_info['name'])
+    ac.setSize(
+            app_window, 
+            app_info['width'], 
+            app_info['height']
+        )
+    background = "apps/python/trackconditions/img/bg.png"
+    ac.setBackgroundTexture(app_window, background)
+    ac.setBackgroundOpacity(app_window, 0)
+    ac.drawBorder(app_window, 0)
+    ac.setTitle(app_window, app_info['title'])
     ac.setIconPosition(app_window, 0, -10000)
+
+    # run onFormRender every render call
     ac.addRenderCallback(app_window, onFormRender)
 
+    # Initialize custom font
+    ac.initFont(0, "Roboto Light", 0, 0)
+    # Calculate fontsize based on padding and app height
+    fontsize = (app_info['height'] * (1 - (2 + 3 * 0.5) * app_info['padding'])) / 4
 
+    # Start the left-aligned text labels
+    labels['grip_text'] = ac.addLabel(app_window, "GRIP:")
+    labels['wind_text'] = ac.addLabel(app_window, "WIND:")
+    labels['road_text'] = ac.addLabel(app_window, "ROAD:")
+    labels['air_text'] = ac.addLabel(app_window, "AIR:")
 
-    # Temporary code whilst developing the app
-    dev_window = ac.newApp("TC Dev Mode")
-    ac.setSize(dev_window, 300, 300)
-    ac.setBackgroundOpacity(dev_window, 0.5)
-    ac.setTitle(dev_window, "TC Dev Mode")
-    ac.addRenderCallback(dev_window, onFormRender_dev)
+    # Apply positioning, color, font, size and alignment to all data labels
+    text_labels_list = ['grip_text', 'wind_text', 'road_text', 'air_text']
+    for n, key in enumerate(text_labels_list):
+        ac.setPosition(
+            labels[key],
+            app_info['padding'] * app_info['height'],
+            (n - 0.35) * fontsize + (1 + 0.6 * n) * app_info['padding'] * app_info['height'])
+        ac.setFontSize(labels[key], fontsize)
+        ac.setCustomFont(labels[key], "Roboto Light", 0, 0)
+        ac.setFontColor(labels[key], 0.9, 0.9, 0.9, 1)
 
-    label_wind_direction = ac.addLabel(dev_window, "Wind Dir (deg)")
-    ac.setPosition(label_wind_direction, 10, 20)
-    ac.setFontSize(label_wind_direction, 24)
+    # Start right-aligned data labels
+    labels['grip_data'] = ac.addLabel(app_window, "X%")
+    labels['wind_data'] = ac.addLabel(app_window, "Xkmh")
+    labels['road_data'] = ac.addLabel(app_window, "XC")
+    labels['air_data'] = ac.addLabel(app_window, "XC")
 
-    label_car_direction = ac.addLabel(dev_window, "Car Dir (deg)")
-    ac.setPosition(label_car_direction, 10, 60)
-    ac.setFontSize(label_car_direction, 24)
-
-    label_rel_direction = ac.addLabel(dev_window, "Rel Dir (deg)")
-    ac.setPosition(label_rel_direction, 10, 100)
-    ac.setFontSize(label_rel_direction, 24)
+    # Apply positioning, color, font, size and alignment to all data labels
+    data_labels_list = ['grip_data', 'wind_data', 'road_data', 'air_data']
+    for n, key in enumerate(data_labels_list):
+        ac.setPosition(
+            labels[key],
+            app_info['height'] * (app_info['aspect_ratio'] - 1 - app_info['padding']),
+            (n - 0.35) * fontsize + (1 + 0.6 * n) * app_info['padding'] * app_info['height'])
+        ac.setFontSize(labels[key], fontsize)
+        ac.setCustomFont(labels[key], "Roboto Light", 0, 0)
+        ac.setFontColor(labels[key], 0.9, 0.9, 0.9, 1)
+        ac.setFontAlignment(labels[key], "right")
 
 
 def acUpdate(deltaT):
     """Run every physics tick of Assetto Corsa. """
-    global data_timer_60_hz, data_timer_0p1_hz
-    global calc_timer_60_hz
-
+    global app_info
     global ac_data
     global arrow
 
-    global APP_HEIGHT, APP_ASPECT_RATIO
+    global data_timer_60_hz, data_timer_0p1_hz
+    global calc_timer_60_hz
 
     #Update timers
     data_timer_60_hz += deltaT
@@ -108,7 +157,7 @@ def acUpdate(deltaT):
         ac_data['wind_speed'] = ac.getWindSpeed()
         ac_data['air_temp'] = info.physics.airTemp
         ac_data['road_temp'] = info.physics.roadTemp 
-        ac_data['track_grip'] = info.graphics.surfaceGrip
+        ac_data['track_grip'] = info.graphics.surfaceGrip * 100
 
     # Calculations at 60 hz
     if calc_timer_60_hz > PERIOD_60_HZ:
@@ -119,8 +168,11 @@ def acUpdate(deltaT):
         # Calculate wind direction relative to car heading direction.
         arrow['angle'] = ac_data['wind_direction'] - ac_data['car_direction']
 
-        arrow['origin'] = {'x': APP_HEIGHT * APP_ASPECT_RATIO - (APP_HEIGHT / 2), 'y': APP_HEIGHT / 2}
-        arrow['radius'] = APP_HEIGHT * 0.4
+        arrow['origin'] = {'x': app_info['height'] 
+                                * app_info['aspect_ratio'] 
+                                - (app_info['height'] / 2), 
+                           'y': app_info['height'] / 2}
+        arrow['radius'] = app_info['height'] * (0.5 - app_info['padding'])
         arrow['width'] = 0.5
 
         # If the wind speed is negligible (near 0), set angle to 0.
@@ -133,24 +185,28 @@ def acUpdate(deltaT):
         arrow['xy_bot_center'] = polar_to_cartesian_coords(arrow['origin'], arrow['radius'] * 0.5, arrow['angle'] + math.pi)
         arrow['xy_bot_left'] = polar_to_cartesian_coords(arrow['origin'], arrow['radius'], arrow['angle'] + math.pi + arrow['width'])
 
-        # Arrow coloring from green (headwind) to yellow (sidewind) to red (tailwind).
-        # arrow['angle'] can take on values between [-2pi, +2pi]
-        # needs to be remapped from [-2pi, 0, 2pi] to [0, 1, 0] in a linear way
-        arrow['color_shift'] = 1 - abs((abs(arrow['angle'])/math.pi - 1))
-
+        # Arrow coloring
         if ac_data['wind_speed'] < 0.1:
+            # With no (neglible) wind, make arrow grey.
             arrow['rgba'] = {'r': 0.6, 
                              'g': 0.6, 
                              'b': 0.6, 
                              'a': 0.6}
         else:
+            # Arrow coloring from green (headwind) to yellow (sidewind) to red (tailwind).
+            # arrow['angle'] can take on values between [-2pi, +2pi]
+            # needs to be remapped from [-2pi, 0, 2pi] to [0, 1, 0] in a linear way
+            arrow['color_shift'] = 1 - abs((abs(arrow['angle'])/math.pi - 1))
+
             if arrow['color_shift'] < 0.5:
+                # From north to east/west shift color from green to yellow.
                 red_amount = 2 * arrow['color_shift']
                 arrow['rgba'] = {'r': red_amount, 
                                  'g': 1, 
                                  'b': 0, 
                                  'a': 1}
             else:
+                # From east/west to south shift color from yellow to red.
                 green_amount = 1 - (arrow['color_shift'] - 0.5) * 2
                 arrow['rgba'] = {'r': 1, 
                                  'g': green_amount, 
@@ -160,18 +216,18 @@ def acUpdate(deltaT):
 
 def onFormRender(deltaT):
     """Run every rendered frame of Assetto Corsa. """
+    # When the user moves the window, the opacity is reset to default.
+    # Therefore, opacity needs to be set to 0 every frame.
+    ac.setBackgroundOpacity(app_window, 0)
+
+    # Update text data lables
+    ac.setText(labels['grip_data'], "{:.1f}%".format(ac_data['track_grip']))
+    ac.setText(labels['wind_data'], "{:.0f}kmh".format(ac_data['wind_speed']))
+    ac.setText(labels['road_data'], "{:.0f}C".format(ac_data['road_temp']))
+    ac.setText(labels['air_data'], "{:.0f}C".format(ac_data['air_temp']))
+
     # Draw wind arrow indicator
     draw_wind_indicator()
-
-
-def onFormRender_dev(deltaT):
-    """Run every rendered frame of Assetto Corsa. """
-    global label_wind_direction, label_car_direction, label_rel_direction
-    global ac_wind_direction, ac_car_direction, ac_rel_direction
-
-    ac.setText(label_wind_direction, "Wind Dir: {:.1f}".format(data_timer_0p1_hz))
-    ac.setText(label_car_direction, "Car Dir: {:.2f}".format(ac_data['car_direction']))
-    ac.setText(label_rel_direction, "Rel Dir: {:.2f}".format(arrow['angle']))
 
 
 def draw_wind_indicator():
@@ -189,6 +245,7 @@ def draw_wind_indicator():
     ac.glVertex2f(arrow['xy_bot_right']['x'], arrow['xy_bot_right']['y'])
     ac.glVertex2f(arrow['xy_bot_center']['x'], arrow['xy_bot_center']['y'])
     ac.glVertex2f(arrow['xy_bot_left']['x'], arrow['xy_bot_left']['y'])
+    # Done with drawing geometry
     ac.glEnd()
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
 # Track Conditions
 
-This is an app for Assetto Corsa.
+This is an app for Assetto Corsa, a racing simulator built by Kunos Simulazioni. It provides the user with a live wind direction indicator, as well as the wind speed, ambient temperature, track temperature and track grip state. 
 
-It provides the user with a wind direction indicator, as well as the wind speed, ambient temperature and track state. 
+![Screenshot](https://i.imgur.com/N8jDF3z.png)
+
+## Instructions
+
+The app can be installed by pasting the content of the .zip file in the main root folder of Assetto Corsa. The size of the app can be tweaked in the config file.
+
+## Functionality
+
+The app provides the use with a live wind indicator. The indictator shows the wind direction relative to the direction the car is facing. The wind indicator is color-coded, showing green when the driver experiences headwind, yellow for a crosswind and red for a tailwind. A headwind produces more downforce and allows the driver to brake later and carry more speed through a corner.
